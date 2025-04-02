31c9a275257a1fd047abbf4219d92948ebeb4d80
Anuj Kumar <anujvermaksj@gmail.com>
2025-04-02 15:32:07 +0530

    si calc component.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import SimpleInterestCalculator from './SimpleInterestCalculator';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SimpleInterestCalculator />
   </StrictMode>,
 )
